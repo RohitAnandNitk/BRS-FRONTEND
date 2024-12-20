db8bcfcfe58999d68738335911bb72f0d2913885
Rohit Anand <147619404+RohitAnandNitk@users.noreply.github.com>
2024-12-20 22:14:32 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,18 +17,9 @@ const  Footer = () => {
        </div>
        <div>
           <h1>ABOUT</h1> 
-           <p>By MCA'26 Batch NITK</p>
-           <p>Members : &nbsp;
-                  <Link to ="https://www.linkedin.com/in/ra157/" className='link'>Rohit</Link>,&nbsp;
-                  <Link to ="https://www.linkedin.com/in/piyush-patel-03236b250/" className='link'>Piyush</Link>,&nbsp;
-                  <Link to ="https://www.linkedin.com/in/ujjwal-panchal-705aa4284/" className='link'>Ujjwal</Link>,&nbsp;
-                  <br></br>
-                  <Link to ="https://www.linkedin.com/in/pranjalpatidar01/" className='link'>Pranjal</Link>,&nbsp;
-                  <Link to ="https://www.linkedin.com/in/shubh-goel-383763295/" className='link'>Shubh</Link>,&nbsp;
-                  <Link to ="https://www.linkedin.com/in/palak-purwar01/" className='link'>Palak</Link>,&nbsp;
-                  <Link to ="https://www.linkedin.com/in/rohan-roy-yash-5205861b5/" className='link'>Rohan</Link>,&nbsp;
-                  <Link to ="https://www.linkedin.com/in/saikat-dey-1648562b0/" className='link'>Saikat</Link>&nbsp;
-            </p>
+           
+           <p><Link to ="https://www.linkedin.com/in/ra157/" className='link'>Rohit</Link>,&nbsp;</p>
+            <p>By MCA'26 Batch NITK</p>
        </div>
        
     </div>
